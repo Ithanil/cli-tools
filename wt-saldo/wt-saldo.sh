a32283bc502a7f2de3a1a11d443c088b4dc77499
Jan Kessler <jakessle@t-online.de>
2023-02-07 18:05:38 +0100

    basically working wt-saldo

--- wt-saldo/wt-saldo.sh
+++ wt-saldo/wt-saldo.sh
@@ -7,14 +7,20 @@ toMinutes () {
     echo "$TIME_H * 60 + $TIME_M" | bc
 }
 
+
+
 WT_BEGIN="$1"
 WT_BREAK="$2"
+WT_DEBT="$3"
+[[ -z $WT_DEBT ]] && WT_DEBT="7:38"
 
 WT_BEGIN_MIN=$(toMinutes $WT_BEGIN)
 WT_BREAK_MIN=$(toMinutes $WT_BREAK)
-WT_DEBT_MIN=$(toMinutes "7:48")
+WT_DEBT_MIN=$(toMinutes $WT_DEBT)
+WT_CUR_MIN=$(toMinutes `date +"%H:%M"`)
 
-
-echo $WT_BEGIN_MIN
-echo $WT_BREAK_MIN
-echo $WT_DEBT_MIN
+#echo $WT_BEGIN_MIN
+#echo $WT_BREAK_MIN
+#echo $WT_DEBT_MIN
+#echo $WT_CUR_MIN
+echo "$WT_DEBT_MIN - ($WT_CUR_MIN - $WT_BEGIN_MIN) + $WT_BREAK_MIN" | bc
